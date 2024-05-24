b21193e609cdcf596b7da994fe94e0029a7f1853
Ankaraar <aaleksov096@gmail.com>
2024-05-24 14:27:48 +0300

    Добавленная инлайн клава

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher, types, executor
-from config import TELEGRAM_TOKEN
 from keyboard.keyboards import get_keyboard_1, get_keyboard_2
-
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 api_key = '6767940798:AAGA8rRP9qoY_gRmlqLAfX0UvInHM8adKJI'
 
 bot = Bot(token= api_key)
 dp = Dispatcher(bot)
 
-
+keyboard_inline = InlineKeyboardMarkup(row_width= 1)
+but_inline = InlineKeyboardButton('Посмотреть', url='https://www.purina.ru/cats/getting-a-new-cat/finding-the-right-cat-for-me/the-most-beautiful-cats')
+but_inline2 = InlineKeyboardButton('Посмотреть', url='https://www.purina.ru/cats/getting-a-new-cat/finding-the-right-cat-for-me/the-most-beautiful-cats')
+keyboard_inline.add(but_inline, but_inline2)
 
 async def set_commands(bot: Bot):
     commands = [
@@ -26,7 +28,7 @@ async def start(message: types.Message):
 
 @dp.message_handler(lambda message: message.text == 'Отправь фото кота')
 async def button_1_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo= 'https://wp-s.ru/wallpapers/0/17/529196211350601/ryzhij-kotik-krupnym-planom.jpg', caption='Вот тебе кот!')
+    await bot.send_photo(message.chat.id, photo= 'https://wp-s.ru/wallpapers/0/17/529196211350601/ryzhij-kotik-krupnym-planom.jpg', caption='Вот тебе кот!', reply_markup=keyboard_inline)
 
 @dp.message_handler(lambda message: message.text == 'Перейти на след. клаву')
 async def button_2_click(message: types.Message):
@@ -62,5 +64,3 @@ async def on_startup(dispatcher):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates= True, on_startup= on_startup)
-
-
